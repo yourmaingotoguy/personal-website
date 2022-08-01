059526ffc7fb85c7e63e7e14c7204ea8131ac402
yourmaingotoguy <khalid.n.mohamed@outlook.com>
2022-08-01 17:43:37 -0400

    Bootstrap frontend Vue project and dockerize container

---
+++ frontend/src/App.vue
@@ -0,0 +1,13 @@
+<script setup>
+import { RouterView } from "vue-router";
+import TopNavigation from "./components/TopNavigation.vue";
+</script>
+
+<template>
+  <div class="container mx-auto">
+    <top-navigation />
+    <RouterView />
+  </div>
+</template>
+
+<style scoped></style>
---
+++ frontend/src/components/TopNavigation.vue
@@ -0,0 +1,15 @@
+<script>
+export default {
+  data() {
+    return {
+      title: "Top Navigation",
+    };
+  },
+};
+</script>
+
+<template>
+  <h1 class="text-3xl font-bold">{{ title }}</h1>
+</template>
+
+<style scoped></style>
